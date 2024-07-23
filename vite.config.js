d55c1044de1c39aefc04e1265c3567a681b8dafc
ggertzog <musienko.roma@mail.ru>
2024-07-23 15:43:04 +0300

    fix config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: { 
+    base: '/TravelAsk/',
+    build: {
         outDir: 'build'
     },
     plugins: [react()],
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/TravelAsk/',
+  build: { 
+      outDir: 'build'
+  },
   plugins: [react()],
   resolve: {
     alias: {
